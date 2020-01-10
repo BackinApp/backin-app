bcac700691ae6a8e5aa26732f8fd81877df8c0fb
Giovanni D <giovanniyatze@gmail.com>
2020-01-10 02:03:01 -0600

    Add techs app and models definition

---
+++ backin/techs/models.py
@@ -0,0 +1,29 @@
+from django.utils.timezone import now
+from django.db import models
+
+
+class Tech(models.Model):
+    name = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        default='')
+    code_name = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        default='')
+    display_name = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        default='')
+    oficial_doc = models.TextField(
+        blank=True,
+        null=True)
+    active = models.BooleanField(
+        blank=False,
+        null=False,
+        default=True)
+    created = models.DateTimeField(default=now, editable=False)
+    last_change = models.DateTimeField(default=now, editable=True)
