5036538ced501c98532abd5e4745ff254e859c4b
Giovanni D <giovanniyatze@gmail.com>
2020-01-10 02:27:37 -0600

    Add DBEngine views.py

--- backin/dbengine/views.py
+++ backin/dbengine/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-# Create your views here.
+from .models import DBEngine
+from .serializers import DBEngineSerializer
+
+
+class DBEngineView(viewsets.ModelViewSet):
+    queryset = DBEngine.objects.all()
+    serializer_class = DBEngineSerializer
+    authentication_classes = [JSONWebTokenAuthentication]
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
