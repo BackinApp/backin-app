9dc9759802ed447040e4e25707a40ff524448abb
Giovanni D <giovanniyatze@gmail.com>
2020-01-10 02:06:54 -0600

    Add techs views, only get mock added, pending all the CRUD operations implementation

--- backin/techs/views.py
+++ backin/techs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+# from rest_framework.response import Response
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-# Create your views here.
+from .models import Tech
+from .serializers import TechSerializer
+
+
+class TechView(viewsets.ModelViewSet):
+    queryset = Tech.objects.all()
+    serializer_class = TechSerializer
+    authentication_classes = [JSONWebTokenAuthentication]
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
