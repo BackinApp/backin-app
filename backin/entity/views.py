8f618c426124e90b28bb1881aeafe29865004420
Giovanni D <giovanniyatze@gmail.com>
2020-01-10 02:46:20 -0600

    Add entity views.py definition and entity tests.py comment for flake8 suggestion fix

--- backin/entity/views.py
+++ backin/entity/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+# from rest_framework.response import Response
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-# Create your views here.
+from .models import Entity
+from .serializers import EntitySerializer
+
+
+class EntityView(viewsets.ModelViewSet):
+    queryset = Entity.objects.all()
+    serializer_class = EntitySerializer
+    authentication_classes = [JSONWebTokenAuthentication]
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
