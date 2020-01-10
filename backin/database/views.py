c309e16b2b07d8f7406c1fec485b29b00d5a24d0
Giovanni D <giovanniyatze@gmail.com>
2020-01-10 02:38:18 -0600

    Add database views.py definition

--- backin/database/views.py
+++ backin/database/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+# from rest_framework.response import Response
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-# Create your views here.
+from .models import Database
+from .serializers import DatabaseSerializer
+
+
+class DatabaseView(viewsets.ModelViewSet):
+    queryset = Database.objects.all()
+    serializer_class = DatabaseSerializer
+    authentication_classes = [JSONWebTokenAuthentication]
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
