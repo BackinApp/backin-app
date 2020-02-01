39a565983e44a710f5a59148e1c1232a25f27660
Giovanni D <giovanniyatze@gmail.com>
2020-02-01 16:51:42 -0600

    Add projects model refactoring

--- backin/dbengine/models.py
+++ backin/dbengine/models.py
@@ -3,27 +3,10 @@ from django.db import models
 
 
 class DBEngine(models.Model):
-    name = models.CharField(
-        max_length=255,
-        blank=True,
-        null=True,
-        default='')
-    code_name = models.CharField(
-        max_length=255,
-        blank=True,
-        null=True,
-        default='')
-    display_name = models.CharField(
-        max_length=255,
-        blank=True,
-        null=True,
-        default='')
-    oficial_doc = models.TextField(
-        blank=True,
-        null=True)
-    active = models.BooleanField(
-        blank=False,
-        null=False,
-        default=True)
+    name = models.CharField(max_length=255, blank=True, null=True, default='')
+    code_name = models.CharField(max_length=255, blank=True, null=True, default='')
+    display_name = models.CharField(max_length=255, blank=True, null=True, default='')
+    oficial_doc = models.TextField(blank=True, null=True)
+    active = models.BooleanField(blank=False, null=False, default=True)
     created = models.DateTimeField(default=now, editable=False)
     last_change = models.DateTimeField(default=now, editable=True)
