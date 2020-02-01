1ff3fe65f9991990e3d5aa548c952f5831f8d92e
Giovanni D <giovanniyatze@gmail.com>
2020-02-01 17:19:44 -0600

    Add projects endpoint for crud

--- backin/projects/urls.py
+++ backin/projects/urls.py
@@ -4,8 +4,9 @@ from django.urls import path
 from .views import ProjectView
 
 urlpatterns = [
-    path('projects', ProjectView.as_view({'get': 'list', 'get': 'retrieve',
-                                          'post':'create', 'put':'update',
-                                          'delete': 'delete'})),
+    path('projects', ProjectView.as_view())
     # path('<int:pk>/', DetailTodo.as_view()),
+    # path('projects', ProjectView.as_view({'get': 'list', 'get': 'retrieve',
+    #                                      'post': 'create', 'put':'update',
+    #                                      'delete': 'delete'})),
 ]
--- backin/projects/views.py
+++ backin/projects/views.py
@@ -1,13 +1,18 @@
+from rest_framework import permissions
+from rest_framework import mixins
+from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-from rest_framework import permissions
 
 from .models import Projects
 from .serializers import ProjectSerializer
 
 
-class ProjectView(viewsets.ModelViewSet):
+# class ProjectView(viewsets.ModelViewSet):
+class ProjectView(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
     queryset = Projects.objects.all()
     serializer_class = ProjectSerializer
     authentication_classes = [JSONWebTokenAuthentication]
@@ -15,5 +20,17 @@ class ProjectView(viewsets.ModelViewSet):
         permissions.IsAuthenticated,
     )
 
-    def perform_create(self, serializer):
-        serializer.save()
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    # def perform_create(self, serializer):
+     #   serializer.save()
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
