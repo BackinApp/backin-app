39d608b6afba21062b078d3174569fa972d6abd9
Giovanni D <giovanniyatze@gmail.com>
2020-02-01 17:23:36 -0600

    Add utils and projects create serializer handler

--- backin/projects/serializers.py
+++ backin/projects/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from .models import Projects
-#from namoxpanel.utils import get_request_token
+from backin.utils import get_request_token
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -9,7 +9,13 @@ class ProjectSerializer(serializers.ModelSerializer):
         model = Projects
         fields = '__all__'
 
-    # @get_request_token
+    @get_request_token
+    def get(self, pk):
+        if validated_data['created_by']:
+            app_obj = Projects.objects.create(**validated_data)
+            return app_obj
+
+    @get_request_token
     def create(self, validated_data):
         if validated_data['created_by']:
             app_obj = Projects.objects.create(**validated_data)
