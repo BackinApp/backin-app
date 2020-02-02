a22d7792375b88f9e2bb96394b647cea62f49394
Giovanni D <giovanniyatze@gmail.com>
2020-02-01 18:34:29 -0600

    Add model refactoring

--- backin/apps/models.py
+++ backin/apps/models.py
@@ -2,6 +2,7 @@ from django.utils.translation import pgettext_lazy
 from django.db import models
 
 from accounts.models import User
+from projects.models import Projects
 
 
 # Main App model creation
@@ -9,23 +10,25 @@ class Apps(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
     version = models.CharField(max_length=255, blank=True, null=True, default='1.0')
-    technology = models.CharField(choices=TECH_CHOICES, max_length=255, blank=True, null=True, default='-')
-    tool = models.CharField(choices=TOOLS_CHOICES, max_length=255, blank=True, null=True, default='-')
+    technology = models.CharField(max_length=255, blank=True, null=True, default='-')
+    tool = models.CharField(max_length=255, blank=True, null=True, default='-')
     stack = models.BooleanField(default=False)
     multiple_db = models.BooleanField(default=False)
-    db_engine = models.CharField(choices=DATABASE_CHOICES, max_length=255, blank=True,
+    db_engine = models.CharField(max_length=255, blank=True,
                                  null=True, default='-')
     db_name = models.CharField(max_length=255, blank=True, null=True)
     db_dump = models.CharField(max_length=255, blank=True, null=True)
     git_repo = models.TextField(blank=True, null=True)
     dockerized = models.BooleanField(default=False)
     admin_panel = models.BooleanField(blank=True, default=False)
-    product = models.CharField(choices=PRODUCT_CHOICES, max_length=255, blank=True, default='-')
-    output = models.CharField(choices=OUTPUT_CHOICES, max_length=255, blank=True, null=True, default='-')
+    product = models.CharField(max_length=255, blank=True, default='-')
+    output = models.CharField(max_length=255, blank=True, null=True, default='-')
     request_log = models.BooleanField(default=True)
     active = models.BooleanField(blank=False, null=False, default=True)
+    project = models.ForeignKey(Projects, related_name='project_app',
+                                null=True, blank=True, on_delete=models.SET_NULL)
     created_by = models.ForeignKey(User, related_name='app_creator',
-                                   null=True, blank=True, on_delete=models.CASCADE)
+                                   null=True, blank=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now=True, editable=False)
     last_update = models.DateTimeField(auto_now=True, editable=True)
 
