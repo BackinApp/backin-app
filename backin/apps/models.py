49185ae2fa36ff7248596e775c4de4b72f492307
Giovanni D <giovanniyatze@gmail.com>
2020-02-01 18:31:59 -0600

    Add App model definition and App view

--- backin/apps/models.py
+++ backin/apps/models.py
@@ -1,1 +1,34 @@
+from django.utils.translation import pgettext_lazy
 from django.db import models
+
+from accounts.models import User
+
+
+# Main App model creation
+class App(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    version = models.CharField(max_length=255, blank=True, null=True, default='1.0')
+    technology = models.CharField(choices=TECH_CHOICES, max_length=255, blank=True, null=True, default='-')
+    tool = models.CharField(choices=TOOLS_CHOICES, max_length=255, blank=True, null=True, default='-')
+    stack = models.BooleanField(default=False)
+    multiple_db = models.BooleanField(default=False)
+    db_engine = models.CharField(choices=DATABASE_CHOICES, max_length=255, blank=True,
+                                 null=True, default='-')
+    db_name = models.CharField(max_length=255, blank=True, null=True)
+    db_dump = models.CharField(max_length=255, blank=True, null=True)
+    git_repo = models.TextField(blank=True, null=True)
+    dockerized = models.BooleanField(default=False)
+    admin_panel = models.BooleanField(blank=True, default=False)
+    product = models.CharField(choices=PRODUCT_CHOICES, max_length=255, blank=True, default='-')
+    output = models.CharField(choices=OUTPUT_CHOICES, max_length=255, blank=True, null=True, default='-')
+    request_log = models.BooleanField(default=True)
+    active = models.BooleanField(blank=False, null=False, default=True)
+    created_by = models.ForeignKey(User, related_name='app_creator',
+                                   null=True, blank=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True, editable=False)
+    last_update = models.DateTimeField(auto_now=True, editable=True)
+
+    class Meta:
+        verbose_name = pgettext_lazy('App model', 'app')
+        verbose_name_plural = pgettext_lazy('Apps model', 'apps')
--- backin/apps/views.py
+++ backin/apps/views.py
@@ -1,2 +1,19 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
+from .models import Apps
+from .serializers import AppSerializer
+
+
+class AppList(generics.ListCreateAPIView):
+    queryset = Apps.objects.all()
+    serializer_class = AppSerializer
+
+
+class AppDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Apps.objects.all()
+    serializer_class = AppSerializer
