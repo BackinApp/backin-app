d90103bc49d6a22adacf7d173a88d861f936788d
Giovanni D <giovanniyatze@gmail.com>
2020-02-01 19:14:21 -0600

    Add app working

--- backin/backin/urls.py
+++ backin/backin/urls.py
@@ -7,19 +7,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-"""
-from .routers import set_auth_routes, set_api_routes, set_cms_routes
-auth_v1_router = set_auth_routes()
-api_v1_router = set_api_routes()
-cms_v1_router = set_cms_routes()
-"""
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.auth_urls')),
-    path('api/v1/', include(('projects.urls',
-                             'apps.urls',
-                             'dbengine.urls'))),
-    #path('api/v1/', include(api_v1_router.urls)),
-    # path('api/v1', include(router.urls)),
+    path('api/v1/', include('projects.urls')),
+    path('api/v1/', include('apps.urls')),
+    #path('api/v1/', include('dbengine.urls')),
 ]
