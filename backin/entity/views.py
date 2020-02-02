82471c81083b93fd8c462d30afd8f0cba6e74ebf
Giovanni D <giovanniyatze@gmail.com>
2020-02-02 09:05:00 -0600

    Add refactoring for entity view

--- backin/entity/views.py
+++ backin/entity/views.py
@@ -1,41 +1,38 @@
-from rest_framework import permissions, mixins, generics, viewsets
+from rest_framework import mixins, generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from .models import Entities, Attributes
 from .serializers import EntitySerializer, AttributeSerializer
 
 
+@permission_classes([IsAuthenticated])
 class EntityList(generics.ListCreateAPIView):
     queryset = Entities.objects.all()
     serializer_class = EntitySerializer
     authentication_classes = [JSONWebTokenAuthentication]
-    permission_classes = (
-        permissions.IsAuthenticated,
-    )
+    #permission_classes = (
+    #    permissions.IsAuthenticated,
+    #)
 
 
+@permission_classes([IsAuthenticated])
 class EntityDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Entities.objects.all()
     serializer_class = EntitySerializer
     authentication_classes = [JSONWebTokenAuthentication]
-    permission_classes = (
-        permissions.IsAuthenticated,
-    )
 
 
+@permission_classes([IsAuthenticated])
 class AttributeList(generics.ListCreateAPIView):
     queryset = Attributes.objects.all()
     serializer_class = AttributeSerializer
     authentication_classes = [JSONWebTokenAuthentication]
-    permission_classes = (
-        permissions.IsAuthenticated,
-    )
 
 
+@permission_classes([IsAuthenticated])
 class AttributeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Attributes.objects.all()
     serializer_class = AttributeSerializer
     authentication_classes = [JSONWebTokenAuthentication]
-    permission_classes = (
-        permissions.IsAuthenticated,
-    )
